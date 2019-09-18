d641f63e47737ee26c766470469e2c6af9a20db5
Taras Shumeiko <shumeikotaras1@gmail.com>
2019-09-18 13:38:42 +0300

    download & view

--- src/app/components/journal-table/journal-table.component.ts
+++ src/app/components/journal-table/journal-table.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 // import { TeacherPanelService } from 'src/app/services/teacher-panel.service';
 import { Store, select } from '@ngrx/store';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription, Observable, Subject } from 'rxjs';
 import { selectCurrentJournal, selectCurrentJournalData } from 'src/app/store/teacher-panel/teacher-panel.selector';
 import { MatTableDataSource } from '@angular/material';
 import { setCurrentLessonIdToStoreAction } from 'src/app/store/teacher-panel/teacher-panel.action';
 import { MarkControllerService } from 'src/app/services/mark-controller.service';
 import { activeMark } from 'src/app/store/marks/marks.selector';
+import { StudentDiaryService } from '../../services/student-diary.service';
+import { takeUntil } from 'rxjs/operators';
+import { HomeworkDialogComponent } from 'src/app/components/homework-dialog/homework-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'webui-journal-table',
@@ -38,12 +43,15 @@ export class JournalTableComponent implements OnInit, OnDestroy {
   getMarksListSubscription: Subscription;
 
   private hoveredLesson = null;
+  private destroyStream$ = new Subject<void>();
 
   constructor(
     // private teacherPanelService: TeacherPanelService,
     private markServ: MarkControllerService,
     private store: Store<object>,
-    private teacherPanelStore: Store<{ teacherPanel }>
+    private teacherPanelStore: Store<{ teacherPanel }>,
+    private studentDiary: StudentDiaryService,
+    public dialog: MatDialog
   ) {
     this.getMarksList$ = this.store.pipe(select(activeMark(true)));
     this.chosenJournal$ = this.teacherPanelStore.pipe(select(selectCurrentJournal));
@@ -75,13 +83,13 @@ export class JournalTableComponent implements OnInit, OnDestroy {
     this.journalArray = Object.values(this.journalData.journal);
     this.journalArray.forEach((item: any) => {
         item.marks.sort((a, b) => {
-          if (new Date(a.dateMark) > new Date(b.dateMark)) 
+          if (new Date(a.dateMark) > new Date(b.dateMark))
             return 1;
-          if (new Date(a.dateMark) < new Date(b.dateMark)) 
+          if (new Date(a.dateMark) < new Date(b.dateMark))
             return -1;
-          if (new Date(a.dateMark) === new Date(b.dateMark)) 
+          if (new Date(a.dateMark) === new Date(b.dateMark))
             return 0;
-            
+
         });
       });
     console.log(this.journalArray)
@@ -116,5 +124,22 @@ export class JournalTableComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.chosenJournalDataSubscription.unsubscribe()
+  }
+
+  downloadFile(lessonId: number): void {
+    this.studentDiary.downloadHomeworkFile(lessonId);
+  }
+
+  openFile(lessonId: number): void {
+    this.studentDiary.openHomeworkFile(lessonId)
+      .pipe(takeUntil(this.destroyStream$))
+      .subscribe(data => {
+        this.dialog.open(HomeworkDialogComponent, {
+          panelClass: 'custom-dialog-container',
+          width: '90vw',
+          height: '80vh',
+          data
+        });
+      });
   }
 }
