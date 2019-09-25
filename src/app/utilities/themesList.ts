9f3c3d49bb8f2c12084aae0b35790a4d1ae1744d
Roman Havryliv <romanhavr@ukr.net>
2019-09-25 11:02:10 +0300

    edited themes colors

--- src/app/utilities/themesList.ts
+++ src/app/utilities/themesList.ts
@@ -1,19 +1,31 @@
 export const dayTheme = {
-  'main-color': '	rgba(223, 230, 233,1.0)',
+  'main-color': '#DFE6E9',
   'light-color': '#fafafa',
   'dark-color': '#00cec9',
   'font-color': 'black',
   'contrast-font-color': 'white',
-  'accent-color': '#F44336',
-  'alternative-accent-color': '#FF4081'
+  'accent-color': '#006050',//'#F44336',
+  'alternative-accent-color': '#FF4081',
+  'hovered-col-row-color': '#f1f3f4', // for teacher-journal
+  'hovered-cell-color': '#d6dadb', // for teacher-journal
+  'box-bg-color': '#f4f6f6', // for teacher-journal
+  'box-shadow-color': '#c2c3c3', // for teacher-journal
+  'homework-icon-color': '#747474', // for teacher-journal
+  'add-icon-color': '#c2c3c3' // for teacher-journal
 };
 
 export const nightTheme = {
-  'main-color': 'rgba(45, 52, 54,1.0)',
-  'light-color': 'rgba(99, 110, 114,1.0)',
+  'main-color': '#2D3436',
+  'light-color': '#636E72',
   'dark-color': 'black',
   'font-color': '#fbf8ea',
   'contrast-font-color': 'white',
   'accent-color': '#03A9F4',
-  'alternative-accent-color': '#FFAB40'
+  'alternative-accent-color': '#FFAB40',
+  'hovered-col-row-color': '#2D3436', // for teacher-journal
+  'hovered-cell-color': '#252A2B', // for teacher-journal
+  'box-bg-color': '#4E5558', // for teacher-journal
+  'box-shadow-color': '#2F3335', // for teacher-journal
+  'homework-icon-color': '#c2c3c3', // for teacher-journal
+  'add-icon-color': '#747474' // for teacher-journal
 };
